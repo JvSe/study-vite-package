c391496a7fc82378fb499955f86e7f1a82b18754
João Vitor Soare <joaovitorsoares12@gmail.com>
2024-01-14 14:59:32 -0300

    teste sem build

---
+++ dist/components/password-input.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+export declare const PasswordInput: import("react").ForwardRefExoticComponent<import("react").InputHTMLAttributes<HTMLInputElement> & {
+    leftIcon?: boolean | undefined;
+} & import("react").RefAttributes<HTMLInputElement>>;

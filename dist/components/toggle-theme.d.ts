305fd4626d218bcd9163d830aa8365a50b39a445
João Vitor Soare <joaovitorsoares12@gmail.com>
2024-01-15 09:55:36 -0300

    test: test do novo build

--- dist/components/toggle-theme.d.ts
+++
@@ -1,5 +0,0 @@
-import { ButtonProps } from '.';
-interface IButtonToggleThemeDTO extends ButtonProps {
-}
-declare function ButtonToggleTheme({ ...rest }: IButtonToggleThemeDTO): import("react/jsx-runtime").JSX.Element;
-export { ButtonToggleTheme };

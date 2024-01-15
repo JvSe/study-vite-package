0870bc65443d75adcee8d66d1aa3deb5974da481
João Vitor Soare <joaovitorsoares12@gmail.com>
2024-01-15 10:47:49 -0300

    remove archives from main

--- dist/components/password-input.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-export declare const PasswordInput: import("react").ForwardRefExoticComponent<import("react").InputHTMLAttributes<HTMLInputElement> & {
-    leftIcon?: boolean | undefined;
-} & import("react").RefAttributes<HTMLInputElement>>;
--- dist/components/spinner.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-export interface SpinnerProps extends React.ButtonHTMLAttributes<HTMLDivElement> {
-}
-export declare const Spinner: ({ className, ...rest }: SpinnerProps) => import("react/jsx-runtime").JSX.Element;

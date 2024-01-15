9e177dac09ef006e3a492ed89489f8444d52b7d0
João Vitor Soare <joaovitorsoares12@gmail.com>
2024-01-15 09:43:28 -0300

    fix: removendo arquivos

--- dist/components/input-with-icon.d.ts
+++
@@ -1,4 +0,0 @@
-import { ElementType } from "react";
-export declare const InputWithIcon: import("react").ForwardRefExoticComponent<import("react").InputHTMLAttributes<HTMLInputElement> & {
-    icon?: ElementType | undefined;
-} & import("react").RefAttributes<HTMLInputElement>>;

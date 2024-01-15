7081a04bf2e19bda73ad3dcde7ad921cc8f918ae
João Vitor Soare <joaovitorsoares12@gmail.com>
2024-01-15 10:23:06 -0300

    setando o arquivo dist

---
+++ dist/icons/flaticons.d.ts
@@ -0,0 +1,6 @@
+import { ComponentProps } from "react";
+type FlatIconProps = ComponentProps<"i">;
+declare const FiEnvelope: ({ ...rest }: FlatIconProps) => import("react/jsx-runtime").JSX.Element;
+declare const FiLock: ({ ...rest }: FlatIconProps) => import("react/jsx-runtime").JSX.Element;
+declare const FiTriangleWarning: ({ ...rest }: FlatIconProps) => import("react/jsx-runtime").JSX.Element;
+export { FiEnvelope, FiLock, FiTriangleWarning };

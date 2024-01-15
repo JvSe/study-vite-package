00453adc2341f340a2d249de694d2ff3072f0bf4
João Vitor Soare <joaovitorsoares12@gmail.com>
2024-01-14 22:36:09 -0300

    setando arquivos

--- dist/lib/index.es.js
+++
@@ -1,7 +0,0 @@
-import { signinValidation as a } from "./form-validations/index.es.js";
-import { c as i, t as n } from "../index-vDNL9MXV.js";
-export {
-  i as cn,
-  a as signinValidation,
-  n as themes
-};
